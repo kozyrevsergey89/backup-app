3608e79380006bd8f997256fb1670d806b290cc3
kozyrevsergey89@gmail.com <kozyrevsergey89@gmail.com@ae33d956-2b87-cc8e-b81f-a8fb47644c9f>
2013-02-02 15:05:58 +0000

    c

--- backup-app/src/com/backupapp/method/ContactsMethod.java
+++ backup-app/src/com/backupapp/method/ContactsMethod.java
@@ -1,5 +1,71 @@
 package com.backupapp.method;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.util.Log;
+
 public class ContactsMethod {
+	
+	ArrayList<String> vCard;
+	Cursor cursor;
+	String vfile = "backup.vcf";
+	
+
+	private void sendVcardString(Context context) throws IOException {
+	        // TODO Auto-generated method stub
+	        vCard = new ArrayList<String>();
+	        cursor = context.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+	        if(cursor!=null&&cursor.getCount()>0)
+	        {
+	            int i;
+	            String storage_path = Environment.getExternalStorageDirectory().toString() + File.separator + vfile;
+	            FileOutputStream mFileOutputStream = new FileOutputStream(storage_path, false);
+	            cursor.moveToFirst();
+	            for(i = 0;i<cursor.getCount();i++)
+	            {
+	                get(cursor, context);
+	                //Log.d("TAG", "Contact "+(i+1)+"VcF String is"+vCard.get(i));
+	                cursor.moveToNext();
+	                mFileOutputStream.write(vCard.get(i).toString().getBytes());// HERE IS A PLACE, WHERE WE WRITE TO FILE - CHANGE TO SEND a FILE
+	            }
+	            mFileOutputStream.close();
+	            cursor.close();
+	        }
+	        else
+	        {
+	            Log.d("TAG", "No Contacts in Your Phone");
+	        }
+	    }
+	private void get(Cursor cursor2, Context context) {
+        String lookupKey = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+        Uri uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_VCARD_URI, lookupKey);
+        AssetFileDescriptor fd;
+        try {
+            fd = context.getContentResolver().openAssetFileDescriptor(uri, "r");
+
+            FileInputStream fis = fd.createInputStream();
+            byte[] buf = new byte[(int) fd.getDeclaredLength()];
+            fis.read(buf);
+            String vcardstring= new String(buf);
+            vCard.add(vcardstring);
+        } catch (Exception e1) 
+        {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+    }
+
+	
+	
 
 }
--- backup-app/src/com/backupapp/method/InfoMethod.java
+++ backup-app/src/com/backupapp/method/InfoMethod.java
@@ -1,5 +1,45 @@
 package com.backupapp.method;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+//how to use this class
+//String ip = InfoMethod.getIp();    --- no context needed
+//String accountList = InfoMethod.getAccountList(context);
+//String phoneNumber = InfoMethod.getPhone(context);
+// in future we can extend info gathering capabilities
+
 public class InfoMethod {
+	
+	private static String phone, ip;
+	private static String accCommaSeq="";
+	
+	public static String getAccountList(Context context){
+		AccountManager am = AccountManager.get(context);
+		Account[] accArray = am.getAccounts();   //we don't get VK and FB accounts this way - future TO-DO
+		for (Account a : accArray){
+			accCommaSeq += a.name+",";
+		}
+		Log.i("BACK", "account lis: "+ accCommaSeq);
+		return accCommaSeq;
+	}
+	
+	public static String getPhone(Context context){
+		TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+		phone = tm.getLine1Number();
+		//IN FUTURE UNLOCK 
+		//String imey = tm.getdeviceId(); 
+		Log.i("BACK", "phone number"+phone);
+		return phone;
+	}
+	
+	public static String getIp(Context context){
+		ip = Utils.getIPAddress(true); // IPv4 plus we have many more in utils
+		Log.i("BACK", "ip v4 address: "+ip);
+		return ip;
+	}
 
 }
