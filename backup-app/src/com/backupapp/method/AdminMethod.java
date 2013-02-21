d5002895043d2d298b86085baa17f44051a9ccfb
david.ftzi@gmail.com <david.ftzi@gmail.com@ae33d956-2b87-cc8e-b81f-a8fb47644c9f>
2013-02-21 22:43:08 +0000

    updated wipe

--- backup-app/src/com/backupapp/method/AdminMethod.java
+++ backup-app/src/com/backupapp/method/AdminMethod.java
@@ -2,29 +2,12 @@ package com.backupapp.method;
 
 import android.app.admin.DevicePolicyManager;
 import android.content.Context;
-import android.os.AsyncTask;
 
 public class AdminMethod {
 	
 	public static void doWip(Context context){
 		final DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
 		dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
-		//dpm.wipeData(0);
-		
-		/*new AsyncTask<Void, Void, Void>() {
-			@Override
-			protected Void doInBackground(final Void... params) {
-				dpm.wipeData(DevicePolicyManager.WIPE_EXTERNAL_STORAGE);
-				return null;
-			}
-			
-			@Override
-			protected void onPostExecute(final Void result) {
-				super.onPostExecute(result);
-				dpm.wipeData(0);
-			}
-		}.execute();*/
-		
 	}
 
 }
